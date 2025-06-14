a5c454be3eaeace3c00ba4fce5d52342b2eaa27b
Ava Wiebe <ava.wiebe04@gmail.com>
2025-06-14 15:17:27 -0400

    Add sounds! More styling!

--- archive/scripts/archive-script.js
+++ archive/scripts/archive-script.js
@@ -1,3 +1,5 @@
+const speechContext = new AudioContext();
+
 class Line {
     content;
     speaker;
@@ -19,13 +21,17 @@ const BASE_TEXT_SPEED = 25;
 
 let DIALOGUE_01 = [
     new Line("...Luna, are you there?", "kyana", 1, "...fffff.fff fff ffffff"),
-    new Line("Lunaaaaaaaaaaaaaaaaaaaaaaa~!", "kyana", 1.5, "wwwwwwwwwwwwwwwwwwwwwwwwwwww"),
+    new Line("Lunaaaaaaaaaaaaaaaaaaaaaaa~!", "kyana", 1.5, "rrrrrrrrrrrrrrrrrrrrrrrrrrrr"),
     new Line("I'm here, dumbass.", "luna", 0.25, "___ wwwww.ssssssss"),
     new Line("Ah, there you are. Enough with that attitude, missy.", "kyana", 1, "__, wwwww ___ ___. _____ ____ ____ iiiiiiiii,______"),
-    new Line("Hmph, fine.", "luna", 0.4, "_____,dddd_"),
+    new Line("Hmph, fine.", "luna", 0.4, "_____.dddd_"),
+    new Line(".....", "kyana", 1, "....."),
+    new Line("Why is your phone destroyed?", "kyana", 0.65, "www __ ____ ppppp-dddddddddd"),
+    new Line("DON'T. ASK.", "luna", 0.4, "tttttt-tttt"),
+    new Line("Okay.......", "kyana", 0.2, "ffffffffffffff"),
 ];
 
-function displayLine(textbox, line = new Line("content not specified.", null, 1, null), speakerDisplay) {
+function displayLine(textbox, line = new Line("content not specified.", null, 1, null), speakerDisplay, voices) {
     return new Promise(resolve => {
         function nextLetter(currentTime) { 
 
@@ -40,20 +46,40 @@ function displayLine(textbox, line = new Line("content not specified.", null, 1,
                 case "b": nxtChar.classList.add("bold"); break; // bold
                 case "i": nxtChar.classList.add("italic"); break; // italic
                 case "a": nxtChar.classList.add("accent"); break; // accent color (purple)
-                case "p": nxtChar.classList.add("accent"); break; // accent2 color (pink)
+                case "p": nxtChar.classList.add("accent2"); break; // accent2 color (pink)
                 case "w": nxtChar.classList.add("wave"); break; // wavy
                 case "s": nxtChar.classList.add("shake"); break; // shaky
+                case "t": nxtChar.classList.add("boldshake"); break; // bold shaky
                 case "f": nxtChar.classList.add("fade"); break; // fade in
                 case "d": nxtChar.classList.add("drop"); break; // drop in
+                case "r": nxtChar.classList.add("rainbow"); break; // massive
                 case ",": delay = BASE_TEXT_SPEED * 4; break;
                 case ".": delay = BASE_TEXT_SPEED * 8; break;
+                case "-": delay = BASE_TEXT_SPEED * 16; break;
             }
-
 
             // Add the character ascii
             nxtChar.textContent = line.content[line.cursor];
 
             textbox.appendChild(nxtChar); // Add the final character with styling to DOM
+
+            // play voice
+            if((line.cursor % Math.ceil(line.speed * 3) == 0 || line.content[line.cursor] == ".") && line.content[line.cursor] != " ") {
+                switch(line.speaker) {
+                    case "kyana":
+                        voices[0].pause();
+                        voices[0].play();
+                        break;
+                    case "luna":
+                        voices[1].pause();
+                        voices[1].play();
+                        break;
+                    case "nyx":
+                        voices[2].pause();
+                        voices[2].play();
+                        break;
+                }
+            }
 
             line.cursor++;
 
@@ -76,6 +102,7 @@ function waitForInput() {
     return new Promise(resolve => {
         $(document).on("keydown click", function(event) {
             if(event.key === " " || event.type === "click") { // Check for spacebar or mouse click
+                if (speechContext.state === "suspended") { speechContext.resume(); } // Initiate the speech context
                 resolve();
                 $(document).off("keydown click");
             }
@@ -88,6 +115,21 @@ window.onload = async function() {
     textbox = document.getElementById("text_box");
     speakerDisplay = document.getElementById("speaker_display");
 
+    const kyanaSpeech = document.getElementById("kyana_speech");
+    const lunaSpeech = document.getElementById("luna_speech");
+    const nyxSpeech = document.getElementById("nyx_speech");
+    const kyanaTrack = speechContext.createMediaElementSource(kyanaSpeech);
+    const lunaTrack = speechContext.createMediaElementSource(lunaSpeech);
+    const nyxTrack = speechContext.createMediaElementSource(nyxSpeech);
+    kyanaTrack.connect(speechContext.destination);
+    lunaTrack.connect(speechContext.destination);
+    nyxTrack.connect(speechContext.destination);
+
+    const voices = [kyanaSpeech, lunaSpeech, nyxSpeech];
+
+    // Force a click to resume the AudioContext
+    await waitForInput();
+
     // Loop through all lines of dialogue
     for(let i = 0; i < dialogue.length; i++) { 
 
@@ -95,7 +137,7 @@ window.onload = async function() {
         textbox.innerHTML = "";
 
         // Display the current line
-        await displayLine(textbox, dialogue[i], speakerDisplay);
+        await displayLine(textbox, dialogue[i], speakerDisplay, voices);
 
         // Wait until click/spacebar to display next line
         await waitForInput();
